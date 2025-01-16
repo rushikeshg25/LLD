cf6157fcd0faf43fc2ae7ba788d4a935db5fc516
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2025-01-16 18:45:07 +0530

    feat:Singelton Rental Systemclass

--- Car-Rental/src/RentalSystem.java
+++ Car-Rental/src/RentalSystem.java
@@ -1,6 +1,39 @@
+import payment.PaymentProcessor;
+import payment.emiPaymentProcessor;
+
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class RentalSystem {
-    List<Car> allCars;
+    private final Map<String, Car> cars;
+    private final Map<String, Reservation> reservations;
+    private final PaymentProcessor paymentProcessor;
+    private static RentalSystem instance;
+
+    private RentalSystem(){
+        cars=new ConcurrentHashMap<>();
+        reservations=new ConcurrentHashMap<>();
+        paymentProcessor=new emiPaymentProcessor();
+    }
+
+    public static  synchronized RentalSystem getInstance(){
+        if(instance == null){
+            instance=new RentalSystem();
+        }
+        return instance;
+    }
+    public void addCar(Car car){
+        cars.put(car.getNum(),car);
+    }
+
+    public void removeCar(String carNum){
+        cars.remove(carNum);
+    }
+
+    public List<Car> searchCars(String maker,String name, ){}
+
+    private boolean isCarAvailable(){}
+
 
 }
