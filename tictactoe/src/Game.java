151894d67e8eb7b2b54de2d937e3129bcca1965e
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2025-01-19 16:08:40 +0530

    finish: Tic Tac Toe

--- tictactoe/src/Game.java
+++ tictactoe/src/Game.java
@@ -17,8 +17,46 @@ public class Game {
         currentPlayer=(currentPlayer==p1)?p2:p1;
     }
 
-    private int getInput(String msg){
-        Scanner scanner=new Scanner(System.in);
-        
+    public void play() {
+        board.printBoard();
+
+        while (!board.isFull() && !board.hasWinner()) {
+            System.out.println(currentPlayer.getName() + "'s turn.");
+            int row = getValidInput("Enter row (0-2): ");
+            int col = getValidInput("Enter column (0-2): ");
+
+            try {
+                board.makeMove(row, col, currentPlayer.getSymbol());
+                board.printBoard();
+                nextTurn();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        if (board.hasWinner()) {
+            nextTurn();
+            System.out.println(currentPlayer.getName() + " wins!");
+        } else {
+            System.out.println("It's a draw!");
+        }
+    }
+
+    private int getValidInput(String message) {
+        Scanner scanner = new Scanner(System.in);
+        int input;
+
+        while (true) {
+            System.out.print(message);
+            if (scanner.hasNextInt()) {
+                input = scanner.nextInt();
+                if (input >= 0 && input <= 2) {
+                    return input;
+                }
+            } else {
+                scanner.next();
+            }
+            System.out.println("Invalid input! Please enter a number between 0 and 2.");
+        }
     }
 }
